4ae8dae8574b9dc61504a365def6406ef103faff
Jeremy Sher <OverlappingElvis@gmail.com>
2015-08-17 21:38:06 -0700

    Update package.json.

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
 var q = require('q');
 var _ = require('underscore');
 var os = require('os');
+var stdin = require('get-stdin');
 
 var delimiter = '<<<break>>>';
-var inputFilename = process.argv[2] || 'captions.sbv';
-var outputFilename = _(inputFilename.split('.sbv')).first() + '.txt';
 
-var readFile = function() {
-	var deferred = q.defer();
-	fs.readFile(inputFilename, function(err, data) {
-		if (err) {
-			deferred.reject(err);
-		}
-		deferred.resolve(data.toString());
-	});
-	return deferred.promise;
-};
+stdin(function(filename) {
+	readFile()
+		.then(transformFile)
+		.then(writeFile)
+		.then(function() {
+			console.log('Done.');
+		})
+		.fail(function(err) {
+			throw err;
+		});
 
-var writeFile = function(data) {
-	var deferred = q.defer();
-	fs.writeFile(outputFilename, data, function(err) {
-		if (err) {
-			deferred.reject(err);
-		}
-		deferred.resolve();
-	});
-	return deferred.promise;
-};
+	function readFile() {
+		var deferred = q.defer();
+		fs.readFile(filename, function(err, data) {
+			if (err) {
+				deferred.reject(err);
+			}
+			deferred.resolve(data.toString());
+		});
+		return deferred.promise;
+	}
 
-var transformFile = function(file) {
-	var groups = _(file.split('\r\n')).map(function(item) {
-		if (item === '') {
-			return delimiter;
-		}
-		return item;
-	}).join().split(delimiter);
-	var joined = _(groups).map(function(group) {
+	function writeFile(data) {
+		var deferred = q.defer();
+		fs.writeFile(_(filename.split('.sbv')).first() + '.txt', data, function(err) {
+			if (err) {
+				deferred.reject(err);
+			}
+			deferred.resolve();
+		});
+		return deferred.promise;
+	}
+
+	function transformFile(file) {
+		var groups = _(file.split('\r\n')).map(function(item) {
+			if (item === '') {
+				return delimiter;
+			}
+			return item;
+		}).join().split(delimiter);
+		var joined = _(groups).map(function(group) {
 			var join = _.compact(group.split(','));
 			join.splice(1, 1);
 			return join[0] + ',' + _(join).rest().join(' ');
 		});
-	return joined.join(os.EOL);
-};
-
-return readFile()
-	.then(transformFile)
-	.then(writeFile)
-	.then(function() {
-		console.log('Done.');
-	})
-	.fail(function(err) {
-		throw err;
-	});
+		return joined.join(os.EOL);
+	}
+});
